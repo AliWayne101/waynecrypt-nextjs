e948895d953c1c485fe2281216f0c92aeff6b87f
AliWayne101 <alimalikwayne@gmail.com>
2023-05-01 02:45:05 +0500

    checking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
   useEffect(() => {
     const TrackingID: string = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID!;
+    console.log(TrackingID);
     initGA(TrackingID);
   }, [])
   
