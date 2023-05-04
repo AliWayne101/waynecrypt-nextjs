bca9bfde28de2331588f957e8ea720601d7f572d
AliWayne101 <alimalikwayne@gmail.com>
2023-05-04 12:37:19 +0500

    added google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import "@/scss/index.scss";
 import { Raleway, Fira_Code } from 'next/font/google';
+import Script from 'next/script';
 
 const raleway = Raleway({ subsets: ["latin"] });
 const firaCode = Fira_Code({ subsets: ["latin"], weight: ["300", "400", "500", "600", "700"] });
@@ -8,6 +9,17 @@ const firaCode = Fira_Code({ subsets: ["latin"], weight: ["300", "400", "500", "
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID!}`} />
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID!}', {
+  page_path: window.location.pathname,
+  });
+`}
+      </Script>
       <style jsx global>{`:root{
       --raleway: ${raleway.style.fontFamily};
       --fira-code: ${firaCode.style.fontFamily};
