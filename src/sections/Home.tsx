0518251f63ae3db55765dd32e034687b2609cfa8
AliWayne101 <alimalikwayne@gmail.com>
2023-05-06 21:39:20 +0500

    media queries for contact section

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -6,64 +6,64 @@ import { motion } from 'framer-motion';
 function Home() {
   return (
     <div className="hero">
-        <motion.h1
-          className="hero-title"
-          initial={{ opacity: 0, y: 5 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{
-            duration: 0.3,
-            ease: "easeInOut",
-            delay: 0.6
-          }}
-        >Hi, welcome to</motion.h1>
-        <motion.h2
-          className="hero-title-large"
-          initial={{ opacity: 0, y: 5 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{
-            duration: 0.3,
-            ease: "easeInOut",
-            delay: 0.75
-          }}
-        >
-            { Details.webFullName }
-        </motion.h2>
-        <motion.h3 
-          className="hero-title-large hero-title-sub"
-          initial={{ opacity: 0, y: 5 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{
-            duration: 0.3,
-            ease: "easeInOut",
-            delay: 1.05
-          }}
-        >
-            Think, <span className='text-main'>Create</span>, Solve
-        </motion.h3>
-        <motion.p
-          className="hero-text"  
-          initial={{ opacity: 0, y: 5 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{
-            duration: 0.3,
-            ease: "easeInOut",
-            delay: 1.35
-          }}
-        >
-          Empowering your digital presence with innovative web and software solutions.
-        </motion.p>
-        <motion.div 
-          className="hero-button"
-          initial={{ opacity: 0, y: 5 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{
-            duration: 0.3,
-            ease: "easeInOut",
-            delay: 1.65
-          }}
-        >
-            <Button text="Watch Projects" link="/#projects"></Button>
-        </motion.div>
+      <motion.h1
+        className="hero-title"
+        initial={{ opacity: 0, y: 5 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          duration: 0.3,
+          ease: "easeInOut",
+          delay: 0.6
+        }}
+      >Hi, welcome to</motion.h1>
+      <motion.h2
+        className="hero-title-large"
+        initial={{ opacity: 0, y: 5 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          duration: 0.3,
+          ease: "easeInOut",
+          delay: 0.75
+        }}
+      >
+        {Details.webFullName}
+      </motion.h2>
+      <motion.h3
+        className="hero-title-large hero-title-sub"
+        initial={{ opacity: 0, y: 5 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          duration: 0.3,
+          ease: "easeInOut",
+          delay: 1.05
+        }}
+      >
+        Think, <span className='text-main'>Create</span>, Solve
+      </motion.h3>
+      <motion.p
+        className="hero-text"
+        initial={{ opacity: 0, y: 5 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          duration: 0.3,
+          ease: "easeInOut",
+          delay: 1.35
+        }}
+      >
+        Empowering your digital presence with innovative web and software solutions.
+      </motion.p>
+      <motion.div
+        className="hero-button"
+        initial={{ opacity: 0, y: 5 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          duration: 0.3,
+          ease: "easeInOut",
+          delay: 1.65
+        }}
+      >
+        <Button text="Watch Projects" link="/#projects"></Button>
+      </motion.div>
     </div>
   )
 }
